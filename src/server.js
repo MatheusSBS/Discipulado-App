d1c5a8d5dda21dac44a1c0c094c298525bca4d20
Matheus SBS <matheus.soares75@gmail.com>
2022-05-31 23:04:08 -0300

    dotenv

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
+const connectToDatabase = require('./database')
+
+connectToDatabase()
+
 const port = 3010
 
 const app = express()
