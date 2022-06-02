00e62b83c9ec4e1c37bb9c94cbaa1e97e2be555f
Matheus SBS <matheus.soares75@gmail.com>
2022-06-01 23:51:23 -0300

    refatoring routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 
+const discipuloController = require('./controllers/discipuloController')
+
 routes.get('/', (req, res) => res.send('Hello Test'))
+routes.get('/discipulos', discipuloController.index)
 
 module.exports = routes
