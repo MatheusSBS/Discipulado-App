df01bf57d3a0691eea7f718631aa1b2c6bbd2910
Matheus SBS <matheus.soares75@gmail.com>
2022-06-06 23:51:29 -0300

    store function with error message

--- src/controllers/discipuloController.js
+++ src/controllers/discipuloController.js
@@ -1,4 +1,4 @@
-const { res } = require('express')
+const { res, response } = require('express')
 
 const Discipulo = require('../models/Discipulo')
 
@@ -11,4 +11,13 @@ module.exports = {
             res.status(500).json({ error: err.message })
         }
     },
+
+    async store(req, res) {
+        const { nome, data_de_nascimento } = req.body
+
+        if( !nome || !data_de_nascimento ) {
+            return response.status(400).json( error: "Missing nome or data de nascimento.")
+        }
+
+    }
 }
